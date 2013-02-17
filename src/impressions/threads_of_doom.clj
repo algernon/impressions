81bb7db6ed1ec4ea55bcd36fd0abf5443d651536
Gergely Nagy <algernon@madhouse-project.org>
2013-02-18 00:40:14 +0100

    threads-of-doom: Major updates
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- src/impressions/threads_of_doom.clj
+++ src/impressions/threads_of_doom.clj
@@ -27,22 +27,23 @@
     (group
      {:scale 0.5}
      (slide {:x -400 :y 760}
-            (fh1 "Synchronizing data"))
+            (fh1 "Shared data"))
 
-     (slide {:x -400 :y 860}
-            (fh1 "Locking"))
+     (group {}
+      (slide {:x -400 :y 860}
+             (fh1 "Locking"))
 
-     (slide {:x -300 :y 960}
-            (fh2 "Races"))
+      (slide {:x -300 :y 960}
+             (fh2 "Races"))
 
-     (slide {:x -300 :y 1060}
-            (fh2 "Deadlocks"))
+      (slide {:x -300 :y 1060}
+             (fh2 "Deadlocks"))
 
-     (slide {:x -300 :y 1160}
-            (fh2 "Granularity"))
+      (slide {:x -300 :y 1160}
+             (fh2 "Granularity"))
 
-     (slide {:x -300 :y 1260}
-            (fh2 "Two-step dance"))
+      (slide {:x -300 :y 1260}
+             (fh2 "Two-step dance")))
 
      (slide {:x -400 :y 1360}
             (fh1 "Priority inversion"))))
@@ -59,32 +60,34 @@
      {:scale 0.5}
 
      ;; --
-     (slide {:x -600 :y 360}
-            (fh1 "Avoiding shared state"))
+     (group {}
+            (slide {:x -600 :y 360}
+                   (fh1 "Avoiding shared state"))
 
-     (slide {:x -300 :y 460
-             :scale 1.5}
-            (fh2 "Re-entrancy"))
+            (slide {:x -300 :y 460
+                    :scale 1.5}
+                   (fh2 "Re-entrancy"))
 
-     (slide {:x -300 :y 560
-             :scale 1.5}
-            (fh2 "Thread-local storage"))
+            (slide {:x -300 :y 560
+                    :scale 1.5}
+                   (fh2 "Thread-local storage")))
 
      ;; --
-     (slide {:x 500 :y 360}
-            (fh1 "Synchronisation"))
+     (group {}
+            (slide {:x 500 :y 360}
+                   (fh1 "Synchronisation"))
 
-     (slide {:x 800 :y 460
-             :scale 1.5}
-            (fh2 "Mutexes"))
+            (slide {:x 800 :y 460
+                    :scale 1.5}
+                   (fh2 "Mutexes"))
 
-     (slide {:x 800 :y 560
-             :scale 1.5}
-            (fh2 "Atomic ops"))
+            (slide {:x 800 :y 560
+                    :scale 1.5}
+                   (fh2 "Atomic ops"))
 
-     (slide {:x 800 :y 660
-             :scale 1.5}
-            (fh2 "Immutability"))
+            (slide {:x 800 :y 660
+                    :scale 1.5}
+                   (fh2 "Immutability")))
 
      (slide {:x -100 :y 1200}
             (fh1 {:class "xx-large"} "Something different?"))))
@@ -102,112 +105,164 @@
     {:x 2200 :y 3400
      :rotate 80}
 
-    (slide {:id "atoms"}
-           (fh1 {:class "largest"} "atoms"))
+    (slide {:id "primitives"}
+           (fh1 "Shifting computation through time and space"))
 
-    (group
-     {:scale 0.75}
+    (group {}
+           (slide {:scale 0.75
+                   :y -60}
+                  (fh2 "Primitive concurrency types"))
+           (group {:scale 0.75
+                   :x 150
+                   :y 110}
+                  (slide {}
+                         [:p "Evaluated once, caches results"])
+                  (slide {:y 60}
+                         [:p "Blocks on deref"]))
 
-     ;; --
-     (slide {:x -100 :y 160}
-            (fh2 "An atomic operation"))
+           (group
+            {:scale 0.01}
 
-     (slide {:x 0 :y 360}
-            [:ul
-             [:li "One data item to manipulate"]
-             [:li "Like a counter!"]
-             [:li "One single state at a time"]
-             ])))
+            (group {}
+                   (slide {:id "delays"}
+                          (fh1 {:class "largest"} "Delays"))
+                   (group {:scale 0.75
+                           :x -100
+                           :y 160}
 
-   ;; ==
-   (group
-    {:x 500 :y 3000
-     :rotate 260}
+                          (slide {}
+                                 [:p "Computed on demand"])
+                          (slide {:y 80}
+                                 [:p "Opt-in cost"])))
 
-    (slide {:id "refs"}
-           (fh1 {:class "largest"} "refs"))
+            (group {:x 1500
+                    :y 240
+                    :scale 0.75}
+                   (slide {:id "futures"}
+                          (fh1 {:class "largest"} "Futures"))
+                   (group {:scale 0.75
+                           :x -100
+                           :y 160}
 
-    (group
-     {:scale 0.75}
+                          (slide {}
+                                 [:p "Run immediately in the background"])
+                          (slide {:y 80}
+                                 [:p "Optional timeout on deref"])
+                          (slide {:y 160}
+                                 [:p "Shares thread pool with agents"])
+                          (slide {:y 240}
+                                 [:p "Simple concurrency"])))
 
-     ;; --
-     (slide {:x -100 :y 200}
-            (fh2 "Same identity over a period of time"))
+            (group {:x 2500
+                    :y 480
+                    :scale 0.5625}
+                   (slide {:id "promises"}
+                          (fh1 {:class "largest"} "Promises"))
+                   (group {:scale 0.75
+                           :x -100
+                           :y 160}
 
-     (slide {:x 0 :y 460}
-            [:ul
-             [:li "Coordinate several data structures"]
-             [:li "Like an SQL transaction"]
-             [:li "Other threads see the old state"]])))
+                          (slide {}
+                                 [:p "Barren container at init"])
+                          (slide {:y 80}
+                                 [:p "Value needs to be delivered separately"])
+                          (slide {:y 200}
+                                 [:p "Can be used to make callback-based APIs synchronous"]))))
 
-   ;; ==
-   (group
-    {:x 300 :y 1800
-     :rotate 250}
+           (group {:x 600
+                   :y -160
+                   :scale 0.5
+                   :rotate 60}
+                  (slide {:id "primitive-demo"}
+                         (fh1 {:class "largest"} "DEMO!")))))
 
-    (slide {:id "futures"}
-           (fh1 {:class "largest"} "futures"))
+   (group {:x 2200 :y 4000
+           :rotate 80}
+          (slide {:id "reference-types"
+                  :y 800}
+                 (fh1 "Reference types"))
 
-    (group
-     {:scale 0.75}
+          (group {:scale 0.75
+                  :y 900
+                  :x -100}
+                 (slide {}
+                        [:p "Boxes"])
+                 (slide {:y 80}
+                        [:p "deref returns a snapshot"])
+                 (slide {:y 160}
+                        [:p "deref never blocks"])
+                 (slide {:y 240}
+                        [:p "watchers & validators"])
+                 (slide {:y 320}
+                        [:p "coordination & synchronisation"])
 
-     (slide {:x -100 :y 160}
-            (fh2 "async request/response"))
+                 (slide {:x 600
+                         :y 120}
+                        [:table {:border 1}
+                         [:tr {:style "background: #bbb;"}
+                          [:td ""]
+                          [:td "coord"]
+                          [:td "uncoord"]]
+                         [:tr
+                          [:td {:style "background: #bbb;"} "sync"]
+                          [:td "Refs"]
+                          [:td "Atoms"]]
+                         [:tr
+                          [:td {:style "background: #bbb;"} "async"]
+                          [:td {:style "background: black;"} ""]
+                          [:td "Agents"]]])
 
-     (slide {:x 0 :y 360}
-            [:ul
-             [:li "..."]])))
+                 (group {:scale 0.01}
 
-   ;; ==
-   (group
-    {:x -700 :y 1200
-     :rotate 190}
+                        (group {}
+                               (slide {:id "atoms"}
+                                      (fh1 {:class "largest"} "atoms"))
+                               (group {:scale 0.75
+                                       :x -100
+                                       :y 100}
+                                      (slide {}
+                                             [:p "Synchronous"])
+                                      (slide {:y 60}
+                                             [:p "Uncoordinated"])
+                                      (slide {:y 120}
+                                             [:p "compare-and-set"])
+                                      (slide {:y 180}
+                                             [:p "blocks on set"])
+                                      (slide {:y 240}
+                                             [:p "can't coordinate atoms"])))
 
-    (slide {:id "agents"}
-           (fh1 {:class "largest"} "agents"))
-
-    (group
-     {:scale 0.75}
-
-     (slide {:x -100 :y 160}
-            (fh2 "..."))
-
-     (slide {:x 0 :y 360}
-            [:ul
-             [:li "..."]])))
-
-   ;; ==
-   (group
-    {:x -1900 :y 1200
-     :rotate 180}
-
-    (slide {:id "promises"}
-           (fh1 {:class "largest"} "promises"))
-
-    (group
-     {:scale 0.75}
-
-     (slide {:x -100 :y 200}
-            (fh2 "..."))
-
-     (slide {:x 0 :y 360}
-            [:ul
-             [:li "..."]])))
-
-   ;; ==
-   (group
-    {:x -2000 :y 100
-     :rotate 90}
-
-    (slide {:id "for-the-doomed"}
-           (fh1 {:class "largest"} "For the doomed"))
-
-    (group
-     {:scale 0.1}
-
-     (slide {:x 0 :y 0}
-            (fh2 "Executor"))))
-
+                        (group {:x 1200
+                                :y 240
+                                :scale 0.75}
+                               (slide {:id "refs"}
+                                      (fh1 {:class "largest"} "refs"))
+                               (group {:scale 0.75
+                                       :x -100
+                                       :y 100}
+                                      (slide {}
+                                             [:p "Synchronous"])
+                                      (slide {:y 60}
+                                             [:p "Coordinated"])
+                                      (slide {:x 500}
+                                             [:p "No inconsistency"])
+                                      (slide {:x 500
+                                              :y 60}
+                                             [:p "No races with involved refs"])
+                                      (slide {:x 500
+                                              :y 120}
+                                             [:p "No manual locks"])
+                                      (slide {:x 500
+                                              :y 180}
+                                             [:p "No deadlocks"])
+                                      (slide {:y 180}
+                                             [:p "In-transaction values"])))
+                        (group {:x 2400
+                                :y 420
+                                :scale 0.5625}
+                               (slide {:id "agents"}
+                                      (fh1 {:class "largest"} "Agents")))
+                        )))
+   
    ;; ==
    (group
     {:x -3000 :y 100
